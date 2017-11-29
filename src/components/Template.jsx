4f1185f2fb62fc9a4cd3a1df03a3c2a5fe075832
Daniel Jaffa <DJaffa@acosta.com>
2017-11-29 16:49:38 -0500

    login page

--- src/components/Template.jsx
+++ src/components/Template.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Header from './shared/Header.jsx';
 import HomePage from './home/HomePage.jsx';
+import LoginPage from './account/LoginPage.jsx';
 import ProfilePage from './account/ProfilePage.jsx';
 
 export default function Template() {
@@ -10,8 +11,11 @@ export default function Template() {
     <Router>
       <div className="wrapper">
         <Header username="amonymous" />
-        <Route exact path="/" component={HomePage} />
-        <Route exact path="/account/profile/:id" component={ProfilePage} />           
+        <section className="page-content container-fluid">
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/account/login" component={LoginPage} />
+          <Route exact path="/account/profile/:id" component={ProfilePage} />           
+        </section>
       </div>
     </Router>
   );
